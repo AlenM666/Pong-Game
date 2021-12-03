5653e43220e1c27ca33151d32a26e547b5256b84
lolek lolek <llolek278@gmail.com>
2021-12-03 23:26:17 +0100

    added score, movment, keypress

--- main.py
+++ main.py
@@ -41,9 +41,83 @@ bounce_ball.dy = -7
 right_player = 0
 left_player = 0
 
+#score display
+sketch = turtle.Turtle()
+sketch.speed(0)
+sketch.color("white")
+sketch.penup()
+sketch.goto(0, 460)
+sketch.write("Left player : 0    Right player : 0", align="center", font=("Courier", 24,"normal"))
+
+#move player vertacly
+def boardleaup():
+    y = left_board.ycor()
+    y += 40
+    left_board.sety(y)
+def boardleadown():
+    y = left_board.ycor()
+    y -= 40
+    left_board.sety(y)
+def boardlebup():
+    y = right_board.ycor()
+    y += 40
+    right_board.sety(y)
+def boardlebdown():
+    y = left_board.ycor()
+    y -= 40
+    left_board.sety(y)
+
+# Keyboard bindings
+sc.listen()
+sc.onkeypress(boardleaup, "e")
+sc.onkeypress(boardleadown, "x")
+sc.onkeypress(boardlebup, "Up")
+sc.onkeypress(boardlebdown, "Down")
 
 
 while True:
     sc.update()
+
+    bounce_ball.setx(bounce_ball.xcor()*bounce_ball.dx)
+    bounce_ball.setx(bounce_ball.xcor()*bounce_ball.dy)
+
+    #checking borders
+    if bounce_ball.ycor() > 280:
+        bounce_ball.sety(280)
+        bounce_ball.dy *= -1
+
+    if bounce_ball.ycor() < -280:
+        bounce_ball.sety(-280)
+        bounce_ball.dy *= -1
+
+
+    if bounce_ball.xcor() > 500:
+        bounce_ball.goto(0, 0)
+        bounce_ball.dy *= -1
+        left_player += 1
+        sketch.clear()
+        sketch.write("Left_player : {}    Right_player: {}".format(
+                      left_player, right_player), align="center",
+                      font=("Courier", 24, "normal"))
+ 
     continue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
